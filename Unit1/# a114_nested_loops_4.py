6f3c1117926a8ea318a995f1773222e637fee2d3
Ruhi Pasham <ruhi.pasham@outlook.com>
2023-10-05 20:52:29 -0400

    Python Nested Loop Code

---
+++ Unit1/# a114_nested_loops_4.py
@@ -0,0 +1,28 @@
+#   a114_nested_loops_4.py 
+import turtle as trtl
+
+painter = trtl.Turtle()
+painter.penup()
+painter.goto(-200, 0)
+painter.pendown()
+
+x = -200
+y = 0
+move_x = 1
+move_y = 1
+while (x < 0):
+  while (y < 100):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  move_y = -1
+  
+  while (y > 0):
+    x = x + move_x
+    y = y + move_y
+    painter.goto(x,y)
+  move_y = 1
+
+
+wn = trtl.Screen()
+wn.mainloop()
